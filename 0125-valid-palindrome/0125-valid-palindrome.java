70169ef73dce4cd20cb1263b2a815e03ee81d801
Dhruv Pachauri <dhruvpachauri28@gmail.com>
2026-07-31 20:41:10 +0530

    Time: 2 ms (99.32%), Space: 44.4 MB (55.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String st="";
-        for(char ch : s.toCharArray()){
-            if(Character.isLetterOrDigit(ch)){
-                char c=Character.toLowerCase(ch);
-                st+=c;
-            }
-        }
-        int left=0,right=st.length()-1;
+        int left=0,right=s.length()-1;
         while(left<right){
-            if(st.charAt(left)!=st.charAt(right)) return false;
-            left++;
-            right--;
+            char first=s.charAt(left);
+            char last=s.charAt(right);
+            if(!Character.isLetterOrDigit(first)){
+                left++;
+            }
+            else if(!Character.isLetterOrDigit(last)){
+                right--;
+            }
+            else{
+                if(Character.toLowerCase(first)!=Character.toLowerCase(last)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
         }
         return true;
     }
